498f46b82bf6d7e0a4ddc4e3aab723b3f22dd928
chanakia <chanakyaweb@outlook.com>
2020-08-14 17:30:13 -0400

    coding challenge initial commit

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
 APP_HOME=$PWD
 
-python3 "$APP_HOME"/src/consumer_complaints.py "$APP_HOME"/input/complaints.csv "$APP_HOME"/output/report.csv
+python3.8 "$APP_HOME"/src/consumer_complaints.py "$APP_HOME"/input/complaints.csv "$APP_HOME"/output/report.csv
